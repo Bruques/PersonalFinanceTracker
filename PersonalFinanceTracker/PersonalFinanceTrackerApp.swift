99753094d2944c6fcddc2c0dfdd670d75861caaa
Bruno Marques <brunomarques@Brunao-MacBook-Air-3.local>
2025-03-30 18:13:45 -0300

    first commit

---
+++ PersonalFinanceTracker/PersonalFinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceTrackerApp.swift
+//  PersonalFinanceTracker
+//
+//  Created by Bruno Marques on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
