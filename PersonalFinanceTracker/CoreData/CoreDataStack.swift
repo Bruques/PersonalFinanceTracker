2c5c4a3fcd562f5181f2cf5bd8dd5e7a48fda8a4
Bruno Marques <brunomarques@Brunao-MacBook-Air-3.local>
2025-03-31 23:41:40 -0300

    create core data stack to handle with contexts

--- PersonalFinanceTracker/CoreData/CoreDataStack.swift
+++ PersonalFinanceTracker/CoreData/CoreDataStack.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataStack: ObservableObject {
+    static let shared = CoreDataStack()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "GestaoEmprestimoModel")
+        container.loadPersistentStores { _, error in
+            if let error {
+                fatalError("Failed to load persistent stores: \(error.localizedDescription)")
+            }
+        }
+        return container
+    }()
+    
+    private init() {}
+}
+
+extension CoreDataStack {
+    func save() {
+        guard persistentContainer.viewContext.hasChanges else { return }
+        do {
+            try persistentContainer.viewContext.save()
+            print("Salvo com sucesso!")
+        } catch {
+            print("Failed to save the context:", error.localizedDescription)
+        }
+    }
+}
