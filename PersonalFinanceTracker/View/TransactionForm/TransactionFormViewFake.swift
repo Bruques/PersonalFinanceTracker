237c39bb5f37b3a107ca7acaa2be26923aa9845e
Nathan <nathan-batista@hotmail.com>
2025-04-13 02:14:25 -0300

    Rename file to avoid conflicts

--- PersonalFinanceTracker/View/TransactionForm/TransactionFormViewFake.swift
+++ PersonalFinanceTracker/View/TransactionForm/TransactionFormViewFake.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct TransactionFormView: View {
+struct TransactionFormViewFake: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    TransactionFormView()
+    TransactionFormViewFake()
 }
